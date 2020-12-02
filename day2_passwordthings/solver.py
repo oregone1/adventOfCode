3bc9abda4c502594e93ecc26c0b207a856206d5e
oregone1 <henryjames2007@gmail.com>
2020-12-02 10:07:41 -0800

    well it works better now...

--- day2_passwordthings/solver.py
+++ day2_passwordthings/solver.py
@@ -1,10 +1,25 @@
-f = open('day2_passwordthings/passwords_in', 'r')
-#w = open('day2_passwordthings/passwords_out', 'w') # this is bad i know
+import os
 
-counter = 1
+f = open(os.path.realpath('adventOfCode/day2_passwordthings/passwords_in'), 'r')
 
 passwords = f.read()
+checker = []
+counter = 1
+
 
 for password in passwords.split(':'):
-    #print(password)
-    #w.write(password + '\n')
+	#print(password.split('\n'))
+	if '-' in password:
+		checker.append(password)
+	else:
+		checker.append(password)
+
+	counter += 1
+
+	if counter > 2:
+		print(checker[0], checker[1])
+		numbers = str(checker[0]).split('-')
+		min_int = numbers[0]
+		max_int = numbers[1]
+		print(min_int, max_int)
+	#w.write(password + '\n')
