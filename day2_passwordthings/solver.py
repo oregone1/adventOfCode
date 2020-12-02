6c756c7633eb61a7fb523f844e15851c7c712688
oregone1 <henryjames2007@gmail.com>
2020-12-02 13:22:38 -0800

    that error cant matter! im sure it works! for sure!

--- day2_passwordthings/solver.py
+++ day2_passwordthings/solver.py
@@ -3,23 +3,27 @@ import os
 f = open(os.path.realpath('adventOfCode/day2_passwordthings/passwords_in'), 'r')
 
 passwords = f.read()
-checker = []
-counter = 1
+valid_pass = []
+x = 0
 
+def format_passwords(password):
+		counter = 0
+		password = password.split(':')
+		passwordnum = password[0].split('-')
+		checkletter = passwordnum[1].split(' ')[1]
 
-for password in passwords.split(':'):
-	#print(password.split('\n'))
-	if '-' in password:
-		checker.append(password)
-	else:
-		checker.append(password)
+		print(passwordnum[0], passwordnum[1].split(' ')[0])
+		print(password[1])
 
-	counter += 1
+		for letter in password[1]:
+			if letter == checkletter:
+				counter += 1
 
-	if counter > 2:
-		print(checker[0], checker[1])
-		numbers = str(checker[0]).split('-')
-		min_int = numbers[0]
-		max_int = numbers[1]
-		print(min_int, max_int)
-	#w.write(password + '\n')
+		if counter > int(passwordnum[0]) and counter < int(passwordnum[1].split(' ')[0]):
+			print(counter)
+			print('valid password found:',password[1])
+			valid_pass.append(password[1])
+			print('valid passwords',len(valid_pass))
+
+for password in passwords.split('\n'):
+	format_passwords(password)
