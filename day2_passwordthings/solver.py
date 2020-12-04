9e018b1f4f9e6335e3a57f996e3fbbca9ed286d6
oregone1 <henryjames2007@gmail.com>
2020-12-03 20:45:01 -0800

    actually functioning code

--- day2_passwordthings/solver.py
+++ day2_passwordthings/solver.py
@@ -12,6 +12,8 @@ def format_passwords(password):
 		passwordnum = password[0].split('-')
 		checkletter = passwordnum[1].split(' ')[1]
 
+		print(passwordnum[1].split(' ')[1])
+
 		print(passwordnum[0], passwordnum[1].split(' ')[0])
 		print(password[1])
 
@@ -19,11 +21,11 @@ def format_passwords(password):
 			if letter == checkletter:
 				counter += 1
 
-		if counter > int(passwordnum[0]) and counter < int(passwordnum[1].split(' ')[0]):
+		if counter >= int(passwordnum[0]) and counter <= int(passwordnum[1].split(' ')[0]):
 			print(counter)
 			print('valid password found:',password[1])
 			valid_pass.append(password[1])
 			print('valid passwords',len(valid_pass))
 
-for password in passwords.split('\n'):
+for password in passwords.split('\n', 999):
 	format_passwords(password)
